7d457759747f3f50c2eb39e44f6600059fa23a82
seonghyeon shin <139723813+maxpia4@users.noreply.github.com>
2025-01-15 15:55:58 +0900

    fix error

--- app/section/Section1.tsx
+++ app/section/Section1.tsx
@@ -5,6 +5,7 @@ export const Section1 = () => (
   <SectionContainer sectionIdx={1}>
     <div className="flex h-screen flex-col items-center justify-center px-10">
       <div className="text-center">
+      </div>
     </div>
   </SectionContainer>
 );
