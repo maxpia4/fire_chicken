c67eac57413d1359e0925a3b762defa233ae4223
seonghyeon shin <139723813+maxpia4@users.noreply.github.com>
2025-01-15 15:52:10 +0900

    Make basic component

---
+++ app/components/Header.tsx
@@ -0,0 +1,121 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+import { Tooltip } from "react-tooltip";
+import { twMerge } from "tailwind-merge";
+
+const NAV_LIST = [
+  { label: "HOME", url: "section-1" },
+  { label: "ABOUT", url: "section-2" },
+  { label: "HOW TO GET", url: "section-3" },
+  { label: "TOKENOMICS", url: "section-4" },
+  { label: "ROADMAP", url: "section-5" },
+];
+
+const MOBILE_MENU = [{ label: "HOME", url: "/" }, ...NAV_LIST];
+
+export const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen((prev) => !prev);
+
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <>
+      {isMenuOpen && (
+        <div className="mobile-menu absolute top-0 left-0 w-full h-screen bg-gray-800 bg-opacity-75 z-50 flex flex-col justify-center items-center">
+          <nav className="text-white space-y-4">
+            <ul>
+              {MOBILE_MENU.map(({ label, url, isNewTab }, i) => (
+                <li key={`mobile-nav-${i}-${label}`}>
+                  <a
+                    href={url}
+                    target={isNewTab ? "_blank" : "_self"}
+                    className="text-xl"
+                  >
+                    {label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <button
+              className="absolute top-4 right-4 text-white text-2xl"
+              onClick={toggleMenu}
+            >
+              X
+            </button>
+          </nav>
+        </div>
+      )}
+      <header
+        className={twMerge(
+          "flex flex-row justify-between items-center bg-[#611111] backdrop-blur-xl text-white",
+          "h-12 px-5 py-2",
+          "lg:h-16 lg:px-64",
+        )}
+      >
+        <a
+          href={"/"}
+          className="cursor-pointer flex-none w-16 h-full relative lg:w-[12.5rem]"
+        >
+          <Image
+            objectFit={"contain"}
+            src="/logo-header.png"
+            alt="logo of fire chicken"
+            fill
+            unoptimized
+          />
+        </a>
+        <nav className={twMerge("desktop", "flex-1")}>
+          <ul className="flex flex-row justify-center gap-32">
+            {NAV_LIST.map(({ label, url, isNewTab }, i) => (
+              <li
+                className={"hover:scale-125 transition-all duration-300 "}
+                key={`header-nav-${i}-${label}`}
+              >
+                <a
+                  // href={url}
+                  onClick={() => scrollToSection(url)}
+                  target={isNewTab ? "_blank" : ''}
+                  className={twMerge(
+                    url ? "cursor-pointer" : "cursor-default coming-soon",
+                  )}
+                >
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        {/*<button*/}
+        {/*  className={twMerge(*/}
+        {/*    "desktop",*/}
+        {/*    "px-8 py-2 border-[1px] border-white rounded-full font-medium",*/}
+        {/*  )}*/}
+        {/*>*/}
+        {/*  Support*/}
+        {/*</button>*/}
+        <button
+          onClick={toggleMenu}
+          className={twMerge("mobile", "flex-none w-5 h-5 relative")}
+        >
+          <Image
+            src="/burger-menu-icon.png"
+            alt="burger menu for open navigation"
+            fill
+            sizes="2.5rem"
+          />
+        </button>
+
+        <Tooltip anchorSelect=".coming-soon" content="Coming soon!" />
+      </header>
+    </>
+  );
+};
---
+++ app/components/SectionContainer.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+
+const needBG = [1,2,3,4,5];
+const videoSection = [];
+export const SectionContainer = ({
+  children,
+  sectionIdx = 1,
+  className,
+}: {
+  children: React.ReactNode;
+  sectionIdx?: number;
+  className?: string;
+}) => {
+  return (
+      <section
+          id={`section-${sectionIdx}`}
+          className={`text-[#F90C02] font-medium relative flex flex-col w-dvw lg:w-full lg:h-[calc(100dvh-4rem)] lg:snap-center ${sectionIdx > 10 ? "lg:overflow-y-scroll" : "lg:overflow-hidden"} ${className}`}
+      >
+        {children}
+        {2 === 1 ? (
+            <div className="bg-[#020417] absolute inset-0 -z-10 w-full hidden lg:block grid h-screen w-screen grid-cols-6 grid-rows-6">
+                {Array.from({ length: 36 }).map((_, idx) => (
+                    <img
+                        src="/fire_chicken_char.png"
+                        alt="Fire Chicken"
+                    />
+                ))}
+            </div>
+        ) : (
+            <>
+            {sectionIdx && needBG.includes(sectionIdx) ? (
+                <>
+                    {videoSection.includes(sectionIdx) ? (
+                        <video
+                            autoPlay={true}
+                            loop
+                            muted
+                            playsInline
+                            className="absolute inset-0 -z-10 w-full hidden lg:block"
+                        >
+                            <source
+                                src={`/section${sectionIdx}-bg_animation.mp4`}
+                                type="video/mp4"
+                            />
+                        </video>
+                    ) : (
+                        <Image
+                            className="absolute inset-0 -z-10 hidden lg:block"
+                            src={`/section${sectionIdx}-bg.${sectionIdx === 1 ? "png" : "webp"}`}
+                            alt={`background image for section ${sectionIdx}`}
+                            style={{objectFit: "cover", objectPosition: "top"}}
+                            fill
+                            sizes="100dvw"
+                        />
+                    )}
+                    <Image
+                        className="absolute inset-0 -z-10 block lg:hidden"
+                        src={`/section${sectionIdx}-bg-m.webp`}
+                        alt={`background image for mobile section ${sectionIdx}`}
+                        fill
+                        style={{objectFit: "cover", objectPosition: "top"}}
+                        sizes="100dvw"
+                    />
+                </>
+            ) : (
+                <div className="absolute inset-0 -z-10 bg-[#020417]"></div>
+            )}
+        </>)}
+      </section>
+  );
+};
