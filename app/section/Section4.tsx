0faa8cdc672a4672a9250ed1dbdab5d84c0e5d97
seonghyeon shin <139723813+maxpia4@users.noreply.github.com>
2025-01-16 15:46:11 +0900

    Feat: modify section4

--- app/section/Section4.tsx
+++ app/section/Section4.tsx
@@ -3,18 +3,13 @@ import {SectionContainer} from "@/app/components/SectionContainer";
 
 export const Section4 = () => (
   <SectionContainer sectionIdx={4}>
-    <div className="flex flex-1 flex-row items-center justify-center px-10">
-      <img
-          className={twMerge("w-[500px]")}
-          src="/fire_chicken_char.png"
-          alt="Fire Chicken"
-      />
-      <div className="flex-1  p-8 rounded-lg shadow-xl bg-black bg-opacity-70 text-white font-bold">
-        <h1 className="text-4xl font-extrabold mb-6">Tokenomics</h1>
+    <div className="flex flex-1 items-center justify-center">
+      <div className="p-8 rounded-lg shadow-xl bg-black bg-opacity-70 text-white font-bold text-center mx-auto">
+        <h1 className="text-9xl font-extrabold mb-6">Tokenomics</h1>
 
         <div className="space-y-6">
           <div className="p-6 rounded-lg shadow-md">
-            <p className="text-xl">
+            <p className="text-4xl">
               Total Supply: 10 billion tokens.
             </p>
           </div>
