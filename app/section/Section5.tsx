cc0ab31da783c00d7d571c030fa5223c9ee50144
seonghyeon shin <139723813+maxpia4@users.noreply.github.com>
2025-01-16 15:46:38 +0900

    Feat: modify section5

--- app/section/Section5.tsx
+++ app/section/Section5.tsx
@@ -1,47 +1,198 @@
+"use client";
+
+import {useEffect, useState} from "react";
 import { twMerge } from "tailwind-merge";
-import {SectionContainer} from "@/app/components/SectionContainer";
+import { SectionContainer } from "@/app/components/SectionContainer";
 
-export const Section5 = () => (
-  <SectionContainer sectionIdx={5}>
-    <div className="flex flex-1 flex-row items-center justify-center px-10">
-      <div className="flex-1 p-8 rounded-lg shadow-xl bg-black bg-opacity-70 text-white font-bold">
-        <h1 className="text-4xl font-extrabold mb-6">Roadmap: Firechicken Coin's Path to the Throne</h1>
+export const Section5 = () => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const [isFading, setIsFading] = useState(false); // 페이드 애니메이션 상태
 
-        <div className="space-y-8">
-          <div className="p-2 rounded-lg shadow-md">
-            <h2 className="text-2xl font-semibold mb-4">Phase 1: Airdrop Events</h2>
-            <p className="text-xl">
-              Launch the <span className="font-semibold">🔥Spicy Challenge🔥</span> with free airdrops to
-              attract community interest.
-            </p>
-          </div>
+  const [isShaking, setIsShaking] = useState(false); // shake 애니메이션 상태
+  const [isFlyingOut, setIsFlyingOut] = useState(false); // flyOut 애니메이션 상태
 
-          <div className="p-2 rounded-lg shadow-md">
-            <h2 className="text-2xl font-semibold mb-4">Phase 2: Collaboration and Meme Events</h2>
-            <p className="text-xl mb-4">
-              <span className="font-semibold">Spicy Meme Viral Campaign:</span> Users create spicy-themed
-              memes to earn rewards in FC Coin.
-            </p>
-            <div className="text-xl italic space-y-4">
-              <p>"🔥If you can handle the heat, you can achieve wealth!"</p>
-              <p>"🌶️Spiciness equals success!"</p>
-              <p>"The spicy coin, Firechicken, claims the throne!"</p>
+  const handleNextStep = () => {
+    if (currentStep < 5) {
+      // 페이드 아웃 트리거
+      setIsFading(true);
+      setTimeout(() => {
+        // 상태 업데이트 후 페이드 인 트리거
+        setCurrentStep((prevStep) => prevStep + 1);
+        setIsFading(false);
+      }, 300); // 페이드 아웃 지속 시간
+    } else {
+      setIsFading(true);
+      setTimeout(() => {
+        // 상태 업데이트 후 페이드 인 트리거
+        setCurrentStep(1);
+        setIsFading(false);
+      }, 300); // 페이드 아웃 지속 시간
+    }
+  };
+
+  useEffect(() => {
+    if (currentStep === 4) {
+      setIsShaking(true); // Shake 애니메이션 시작
+    }
+    if (currentStep === 5) {
+      setIsShaking(false); // Shake 애니메이션 종료
+      setTimeout(() => {
+        setIsFlyingOut(true); // FlyOut 애니메이션 시작
+      }, 100); // 1초 후에 FlyOut 애니메이션 시작
+    }
+  }, [currentStep]);
+
+  const marginTopValues = ["mt-52", "mt-24", "mt-12", "mt-24"];
+  const currentMarginTop = marginTopValues[currentStep];
+
+  return (
+    <SectionContainer sectionIdx={5}>
+      <div className="relative flex flex-col items-center h-screen px-10"
+           onClick={handleNextStep}
+      >
+        {/* 텍스트 영역 */}
+        {currentStep < 4 ? (<div
+          className={twMerge(
+            "w-full flex flex-col items-center justify-center text-center z-10 transition-all duration-500 ease-in-out",
+            currentMarginTop
+          )}
+        >
+          {currentStep === 0 && (<div
+              className={twMerge(
+                "relative bg-white text-black text-xl font-bold p-4 rounded-xl shadow-xl cursor-pointer w-max",
+                isFading ? "opacity-0 translate-y-4" : "opacity-100 translate-y-0" // 페이드 애니메이션
+              )}
+            >
+              Ready for the roadmap? Click me!
+              {/* 말풍선 꼬리 */}
+              <div
+                className="absolute bottom-[-12px] left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-[12px] border-r-[12px] border-t-[12px] border-t-white border-l-transparent border-r-transparent"/>
             </div>
-          </div>
+          )}
 
-          <div className="p-2 rounded-lg shadow-md">
-            <h2 className="text-2xl font-semibold mb-4">Phase 3: Decentralization</h2>
-            <p className="text-xl">
-              Support Firechicken initiatives to expand the community globally and enable decentralization.
-            </p>
-          </div>
+          {currentStep > 0 && (
+            <div
+              className={twMerge(
+                "w-2/3 p-8 rounded-lg shadow-xl bg-black bg-opacity-70 text-white font-bold transition-all duration-300 ease-in-out",
+                isFading ? "opacity-0 translate-y-4" : "opacity-100 translate-y-0" // 페이드 애니메이션
+              )}
+            >
+              {currentStep === 1 && (
+                <div className="space-y-8">
+                  <div className="p-2 rounded-lg shadow-md">
+                  <h2 className="text-3xl font-semibold mb-4">Phase 1: Airdrop Events</h2>
+                    <p className="text-2xl">
+                      Launch the <span className="font-semibold">🔥Spicy Challenge🔥</span> with free airdrops to
+                      attract community interest.
+                    </p>
+                  </div>
+                </div>
+              )}
+
+              {currentStep === 2 && (
+                <div className="space-y-8">
+                  <div className="p-2 rounded-lg shadow-md">
+                    <h2 className="text-3xl font-semibold mb-4">Phase 2: Collaboration and Meme Events</h2>
+                    <p className="text-2xl mb-4">
+                      <span className="font-semibold">Spicy Meme Viral Campaign:</span> Users create spicy-themed
+                      memes to earn rewards in FC Coin.
+                    </p>
+                    <div className="text-xl italic space-y-4">
+                      <p>"🔥If you can handle the heat, you can achieve wealth!"</p>
+                      <p>"🌶️Spiciness equals success!"</p>
+                      <p>"The spicy coin, Firechicken, claims the throne!"</p>
+                    </div>
+                  </div>
+                </div>
+              )}
+
+              {currentStep === 3 && (
+                <div className="space-y-8">
+                  <div className="p-2 rounded-lg shadow-md">
+                    <h2 className="text-3xl font-semibold mb-4">Phase 3: Be the King</h2>
+                    <p className="text-2xl">
+                      Firechicken, with its expanded ecosystem, shows meme coins the true meaning of spiciness and
+                      becomes the absolute king.
+                    </p>
+                  </div>
+                </div>
+              )}
+
+                <button
+                  className="mt-4 px-6 py-2 bg-orange-500 hover:bg-orange-600 text-white rounded-lg"
+                >
+                  {currentStep < 3 ? "Next Phase" : "Beat up the King!"}
+                </button>
+            </div>
+          )}
+        </div>) : (
+          <>
+            <div
+              className={twMerge(
+                "bg-white text-black text-xl font-bold p-4 rounded-xl shadow-xl cursor-pointer w-max mt-44",
+                isFading ? "opacity-0 translate-y-4" : "opacity-100 translate-y-0" // 페이드 애니메이션
+              )}
+            >
+              {currentStep === 4 ? "Ready for the King? Click me!":"Again?"}
+              {/* 말풍선 꼬리 */}
+              <div
+                className="absolute bottom-[-12px] left-1/2 transform -translate-x-1/2 w-0 h-0 border-l-[12px] border-r-[12px] border-t-[12px] border-t-white border-l-transparent border-r-transparent"/>
+            </div>
+            <div className="absolute top-1/4 flex justify-center gap-4">
+              <img
+                className={twMerge(
+                  "w-[80px]",
+                  isShaking && "animate-shake", // Shake 애니메이션 적용
+                  isFlyingOut && "animate-flyOut1", // FlyOut 애니메이션 적용
+                  !isShaking && !isFlyingOut && "opacity-100 transform scale-100 translate-y-0" // 기본 상태
+                )}
+                src="/othercoin/dodge.png"
+                alt="Dodge Coin"
+              />
+              <img
+                className={twMerge(
+                  "w-[80px]",
+                  isShaking && "animate-shake2", // Shake 애니메이션 적용
+                  isFlyingOut && "animate-flyOut2", // FlyOut 애니메이션 적용
+                  !isShaking && !isFlyingOut && "opacity-100 transform scale-100 translate-y-0" // 기본 상태
+                )}
+                src="/othercoin/fudgepeng.png"
+                alt="Fudge Peng"
+              />
+              <img
+                className={twMerge(
+                  "w-[80px]",
+                  isShaking && "animate-shake", // Shake 애니메이션 적용
+                  isFlyingOut && "animate-flyOut3", // FlyOut 애니메이션 적용
+                  !isShaking && !isFlyingOut && "opacity-100 transform scale-100 translate-y-0" // 기본 상태
+                )}
+                src="/othercoin/pepe.png"
+                alt="Pepe Coin"
+              />
+              <img
+                className={twMerge(
+                  "w-[80px]",
+                  isShaking && "animate-shake2", // Shake 애니메이션 적용
+                  isFlyingOut && "animate-flyOut4", // FlyOut 애니메이션 적용
+                  !isShaking && !isFlyingOut && "opacity-100 transform scale-100 translate-y-0" // 기본 상태
+                )}
+                src="/othercoin/shibainu.png"
+                alt="Shiba Inu"
+              />
+            </div>
+          </>
+
+        )}
+
+        {/* 이미지 영역 */}
+        <div className="absolute bottom-0 left-0 w-full flex justify-center">
+          <img
+            className={twMerge("w-[500px]")}
+            src="/fire_chicken_char2.png"
+            alt="Fire Chicken"
+          />
         </div>
       </div>
-      <img
-        className={twMerge("w-[500px]")}
-          src="/fire_chicken_char.png"
-          alt="Fire Chicken"
-      />
-    </div>
-  </SectionContainer>
-);
+    </SectionContainer>
+  );
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,33 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: {
+  plugins: any[];
+  theme: {
+    extend: { colors: { background: string; foreground: string } };
+    keyframes: {
+      flyOut2: { "100%": { transform: string; opacity: number }; "0%": { transform: string; opacity: number } };
+      flyOut1: { "100%": { transform: string; opacity: number }; "0%": { transform: string; opacity: number } };
+      shake: {
+        "25%": { transform: string };
+        "100%": { transform: string };
+        "0%": { transform: string };
+        "75%": { transform: string };
+        "50%": { transform: string }
+      };
+      shake2: {
+        "25%": { transform: string };
+        "100%": { transform: string };
+        "0%": { transform: string };
+        "75%": { transform: string };
+        "50%": { transform: string }
+      };
+      flyOut4: { "100%": { transform: string; opacity: number }; "0%": { transform: string; opacity: number } };
+      flyOut3: { "100%": { transform: string; opacity: number }; "0%": { transform: string; opacity: number } }
+    };
+    animation: { flyOut2: string; flyOut1: string; shake: string; shake2: string; flyOut4: string; flyOut3: string }
+  };
+  content: string[]
+} = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,6 +40,47 @@ export default {
         foreground: "var(--foreground)",
       },
     },
+    keyframes : {
+      flyOut1: {
+        "0%": { transform: "translateY(0) translateX(0) rotate(0deg)", opacity: 1 },
+        "100%": { transform: "translateY(-1000px) translateX(-1000px) rotate(720deg)", opacity: 0 },
+      },
+      flyOut2: {
+        "0%": { transform: "translateY(0) translateX(0) rotate(0deg)", opacity: 1 },
+        "100%": { transform: "translateY(-1000px) translateX(-500px) rotate(-720deg)", opacity: 0 },
+      },
+      flyOut3: {
+        "0%": { transform: "translateY(0) translateX(0) rotate(0deg)", opacity: 1 },
+        "100%": { transform: "translateY(-1000px) translateX(500px) rotate(720deg)", opacity: 0 },
+      },
+      flyOut4: {
+        "0%": { transform: "translateY(0) translateX(0) rotate(0deg)", opacity: 1 },
+        "100%": { transform: "translateY(-1000px) translateX(1000px) rotate(-720deg)", opacity: 0 },
+      },
+      shake: {
+        "0%": { transform: "translateX(0)" },
+        "25%": { transform: "translateX(-10px)" },
+        "50%": { transform: "translateX(10px)" },
+        "75%": { transform: "translateX(-10px)" },
+        "100%": { transform: "translateX(0)" },
+      },
+      shake2: {
+        "0%": { transform: "translateX(0)" },
+        "25%": { transform: "translateX(10px)" },
+        "50%": { transform: "translateX(-10px)" },
+        "75%": { transform: "translateX(10px)" },
+        "100%": { transform: "translateX(0)" },
+      },
+    },
+    animation: {
+      flyOut1: "flyOut1 0.3s linear 0.3s forwards",
+      flyOut2: "flyOut2 0.3s linear 0.3s forwards",
+      flyOut3: "flyOut3 0.3s linear 0.3s forwards",
+      flyOut4: "flyOut4 0.3s linear 0.3s forwards",
+      shake: "shake 0.2s ease-in-out infinite", // 0.3초 동안 반복되는 진동 효과
+      shake2: "shake2 0.2s ease-in-out infinite", // 0.3초 동안 반복되는 진동 효과
+    },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
