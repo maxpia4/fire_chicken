02d532d66aedc76d71712a46eadddcf944532e2e
seonghyeon shin <64464478+devheyheyhihi@users.noreply.github.com>
2025-06-05 16:24:02 +0900

    Create: section1

--- app/section/Section1.tsx
+++ app/section/Section1.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import {SectionContainer} from "@/app/components/SectionContainer";
+import {Header} from "@/app/components/Header";
+import React from "react";
 
 export const Section1 = () => {
   return (
     <SectionContainer sectionIdx={1}>
+      <Header />
       <div className="flex-1 h-full"></div>
     </SectionContainer>
   );
